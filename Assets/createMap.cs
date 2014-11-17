0f20ca1bf88c78bccb3b2abb94cffe9d2fa16df8
Douglas Vaghetti <douglasvaghetti@gmail.com>
2014-11-17 01:54:12 -0200

    Changes the name of the script file, also adds most of the cube and array generation code. Still not working properly.

---
+++ Assets/createMap.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class createMap : MonoBehaviour {
+	public int size=40;
+	public float blockSize=0.25f;
+	private bool[,] array;
+	public Object empty;
+	public Object wall;
+	private List<Object> cubes;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			createRandomArray();
+			createCubes();
+		}
+
+		if(Input.GetKeyDown(KeyCode.Q)){
+			deleteCubes();
+			filterCubes();
+			createCubes();
+		}
+
+	}
+
+	private void createRandomArray(){
+		array = new bool[size,size];
+		cubes=new List<Object>();
+		for (int x=0; x<size; x++) {
+			for(int y=0;y<size;y++){
+				if(Random.value>0.45f){
+					array[x,y]=false;
+				}else{
+					array[x,y]=true;
+				}
+			}
+		}
+	}
+
+	private void createCubes(){
+		for (int x=0; x<size; x++) {
+			for(int y=0;y<size;y++){
+				if(Random.value>0.45f){
+					cubes.Add(
+						Object.Instantiate(empty,new Vector3(x*blockSize,y*blockSize,0),Quaternion.identity));
+				}else{
+					cubes.Add(
+						Object.Instantiate(wall,new Vector3(x*blockSize,y*blockSize,0),Quaternion.identity));
+				}
+			}
+		}
+	}
+
+	private void filterCubes(){
+		for(int x=1;x<size-1;x++){
+			for(int y=1;y<size-1;y++){
+				filterCube(x,y);
+			}
+		}
+	}
+
+	private void filterCube(int x,int y){
+		int cont =0;
+		for(int xit=-1;xit<=1;xit++){
+			for(int yit=-1;yit<=1;yit++){
+				if(!array[x+xit,y+yit]){
+					cont++;
+				}
+			}
+		}
+		if(cont>=5){
+			array[x,y]=false;
+		}
+		//bDebug.Log("cont = "+cont);
+	}
+
+	private void deleteCubes(){
+		for(int x=0;x<cubes.Count;x++){
+			Object.Destroy(cubes[x]);
+		}
+	}
+
+}
